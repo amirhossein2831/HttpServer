4e28b56a81c6952d140944797d1bc3c42f01ed28
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-18 10:20:41 -0700

    :hammer: chore: create render response to render html

--- src/main/java/org/example/Http/Response.java
+++ src/main/java/org/example/Http/Response.java
@@ -6,6 +6,9 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Response {
 
@@ -27,6 +30,21 @@ public class Response {
     public static void send(HttpExchange exchange, String res) {
         exchange.getResponseHeaders().set("Content-Type", "text/html");
         try {
+            exchange.sendResponseHeaders(200, res.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(res.getBytes());
+            os.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void render(HttpExchange exchange, String filePath) {
+        filePath = "src/main/resources/templates/" + filePath;
+        Path path = Paths.get(filePath);
+        exchange.getResponseHeaders().set("Content-Type", "text/html");
+        try {
+            String res =  Files.readString(path);
             exchange.sendResponseHeaders(200, res.length());
             OutputStream os = exchange.getResponseBody();
             os.write(res.getBytes());
