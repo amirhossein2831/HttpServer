db042e83362f3e370f91a807fa58150716ed267d
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-20 15:40:27 -0700

    :poop: chore: remove some :poop: code

--- src/main/java/org/example/Component/Controller/Controller.java
+++ src/main/java/org/example/Component/Controller/Controller.java
@@ -1,15 +1,5 @@
 package org.example.Component.Controller;
 
-import org.example.Http.Request;
-
 public abstract class Controller {
-    protected Request request;
 
-    public Request getRequest() {
-        return request;
-    }
-
-    public void setRequest(Request request) {
-        this.request = request;
-    }
 }
