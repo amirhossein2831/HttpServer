867bd106807cd1f37e0db00d97730aaa04b3c29c
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-21 13:11:38 -0700

    :hammer: chore: update the user class

--- src/main/java/org/example/Model/User.java
+++ src/main/java/org/example/Model/User.java
@@ -1,13 +1,52 @@
 package org.example.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import org.example.Component.Model.Model;
 
+@Entity
 public class User extends Model {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "age")
     private int age;
+
+    public User() {
+    }
 
     public User(String name, int age) {
         this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 }
