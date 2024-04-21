6398ce02ec46c02ee8d8bafbc70e70df88c31187
a.motaghian <amirhosseinmotaghian0994@gmail.com>
2024-04-21 13:13:26 -0700

    :hammer: chore: update the job class

--- src/main/java/org/example/Model/Job.java
+++ src/main/java/org/example/Model/Job.java
@@ -1,11 +1,29 @@
 package org.example.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import org.example.Component.Model.Model;
 
+@Entity
 public class Job extends Model {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "salary")
     private double salary;
+
+    public Job() {
+
+    }
 
     public Job(String title, String description, double salary) {
         this.title = title;
@@ -13,4 +31,35 @@ public class Job extends Model {
         this.salary = salary;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
